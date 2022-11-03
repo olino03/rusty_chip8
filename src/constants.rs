48d5bf98e9d530350b41c29c4856ae04868a33a5
olino03 <catalinolinovici@gmail.com>
2022-11-03 15:20:21 +0200

    Initialize default values for the interpreter

--- src/constants.rs
+++ src/constants.rs
@@ -2,3 +2,5 @@ pub const ADRESS_SPACE: usize = 4096;
 pub const REGISTER_AMMOUNT: usize = 16;
 pub const SCREEN_ROWS: usize = 64;
 pub const SCRREN_COLUMNS: usize = 32;
+pub const STACK_SIZE: usize = 64;
+pub const PROGRAM_START: usize = 0x200;
